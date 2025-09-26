61f29253ba39061fa9d1469d2f5a7e2a627e71ef
Dilana Gamboa <dilana.gamboa2@gmail.com>
2025-09-25 19:19:19 -0600

    Avance interfaz

---
+++ Assets/Scripts/Menu/CountryHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(PolygonCollider2D))]
+public class CountryHandler : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+    public Color32 oldColor;
+    public Color32 hoverColor;
+    public Color32 startColor;
+    void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        sprite.color = startColor;
+    }
+    void OnMouseEnter()
+    {
+        oldColor = sprite.color;
+        sprite.color = hoverColor;
+    }
+    void OnMouseExit()
+    {
+        sprite.color = oldColor;
+    }
+}
